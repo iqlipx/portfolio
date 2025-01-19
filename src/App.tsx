a06038a5f25c1518a9db45d3b0820cc1bb6e63ee
iqlip <122716618+iqlipx@users.noreply.github.com>
2025-01-20 00:25:20 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -225,9 +225,6 @@ function App() {
             <div className="w-3 h-3 rounded-full bg-yellow-500"></div>
             <div className="w-3 h-3 rounded-full bg-green-500"></div>
           </div>
-          <div className="flex-1 text-center text-sm text-gray-300">
-            Terminal
-          </div>
         </div>
 
         {/* Terminal Content */}
